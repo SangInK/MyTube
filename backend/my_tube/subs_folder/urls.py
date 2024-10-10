e59ffa3279e7aa68528c1319bc54bfada0a619e1
KANAI\kanai <sai6272.ngn@gmail.com>
2024-10-10 14:14:07 +0900

    ++ backend commit
    1. subs_folder 앱 수정
      1) folde() 추가
        >> urls 적용
        >> delete() 추가
    
      2) subscriptions() 수정
        >> post() 추가
        >> delete() 추가

--- backend/my_tube/subs_folder/urls.py
+++ backend/my_tube/subs_folder/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("<int:pk>/<str:nextToken>/", views.subscriptions.as_view()),
-    # path("sub/", views.subscription.as_view()),
-    # path("folder/<int:pk>", views.folder.as_view()),
+    path("folder/<int:pk>", views.folder.as_view()),
     path("folders/", views.folders.as_view()),
 ]
--- backend/my_tube/subs_folder/views.py
+++ backend/my_tube/subs_folder/views.py
@@ -25,7 +25,8 @@ def _get_credentials(request):
 
 def _get_subscriptions(request, nextToken=""):
     """
-    구독중인 youtube 채널 조회
+    구독중인 youtube 채널 조회.
+    google api를 통해 가져온 youtube 채널과 폴더에 저장된 채널 정보 = Subscription의 데이터를 합쳐서 표시
 
     Args:
         nextToken (string): api를 통해 youtube 구독 채널 조회 시 한 번에 모든 채널을 조회할 수 없으므로
@@ -72,6 +73,17 @@ def _get_subscriptions(request, nextToken=""):
     return subs
 
 
+class folder(APIView):
+    def delete(self, request, pk):
+        try:
+            folder = Folder.objects.get(id=pk)
+            folder.delete()
+
+            return Response({"isOk": True}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"isOk": False}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class folders(APIView):
     def get(self, request):
         if "credentials" in request.session:
@@ -110,3 +122,50 @@ class subscriptions(APIView):
         )
 
         return Response(subs, status=status.HTTP_200_OK)
+
+    def post(self, request, pk, nextToken):
+        # 이미 입력된 구독채널이 있으면 삭제
+        # 채널의 폴더 간 이동 기능 없으므로 주석처리
+        # data = [data for data in request.data if "folder" in data]
+
+        # if len(data) > 0:
+        #     filter = Q()
+
+        #     for item in data:
+        #         filter |= Q(subs_id=item["subs_id"])
+
+        #     subs = Subscription.objects.filter(filter)
+        #     subs.delete()
+
+        folder = Folder.objects.get(id=pk)
+        folder_serializer = FolderSerializer(folder)
+
+        data = request.data
+        for item in data:
+            item["folder"] = folder_serializer.data
+
+        serializer = SubscriptionSerializer(data=data, many=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+        return Response(
+            {"isOk": True, "subs": serializer.data}, status=status.HTTP_200_OK
+        )
+
+    def delete(self, request, pk, nextToken):
+        try:
+            filter = Q(folder=pk)
+            filter_channel_id = Q()
+
+            for subs in request.data:
+                filter_channel_id |= Q(subs_id=subs["subs_id"])
+
+            filter &= filter_channel_id
+
+            subs = Subscription.objects.filter(filter)
+            subs.delete()
+
+            return Response({"isOk": True}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
